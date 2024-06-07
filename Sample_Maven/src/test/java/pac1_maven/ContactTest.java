b93b7677e114f6a45d80e8709ddd348cc874e631
samantaray5699 <adityasamantaray845@gmail.com>
2024-06-07 12:50:27 +0530

    s

---
+++ Sample_Maven/src/test/java/pac1_maven/ContactTest.java
@@ -0,0 +1,19 @@
+package pac1_maven;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("createContactTest");
+	
+	}
+
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("modifyContactTest");
+	}
+}
---
+++ Sample_Maven/src/test/java/pac2_maven/OrgTest.java
@@ -0,0 +1,22 @@
+package pac2_maven;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	public class ContactTest {
+
+		@Test
+		public void createOrgTest()
+		{
+			System.out.println("createOrgTest");
+		
+		}
+
+		@Test
+		public void modifyOrgTest()
+		{
+			System.out.println("modifyOrgTest");
+		}
+	}
+}
